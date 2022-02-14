6af6c10e519fa838b3d08ca9bd4b71738cd12ffd
JordanWelsman <jordan.welsman@hotmail.co.uk>
2022-02-14 01:46:09 +0000

    Created second activity.

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myfirstapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user hits 'SEND' */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply { // Intent constructor
+            putExtra(EXTRA_MESSAGE, message) // Add value of 'editTextTextPersonName' to intent
+        }
+        startActivity(intent) // Create instance of DisplayMessageActivity class
     }
 }
