e163cbbc5fbd957f0524343fbbbde32ec1cbe54c
JordanWelsman <jordan.welsman@hotmail.co.uk>
2022-02-14 02:14:10 +0000

    Completed second activity.

--- app/src/main/java/com/example/myfirstapplication/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/DisplayMessageActivity.kt
@@ -2,10 +2,20 @@ package com.example.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+
+        // Get intent that started this activity and extract string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
     }
 }
